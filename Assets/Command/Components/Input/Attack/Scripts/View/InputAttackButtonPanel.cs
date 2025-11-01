f19c4921b3de1997bd62b43799b8f1f5e9da6846
toki1616 <tokigit@gmail.com>
2025-11-01 13:53:05 +0900

    UIに入力している方向とボタンを表示するものを追加

---
+++ Assets/Command/Components/Input/Attack/Scripts/View/InputAttackButtonPanel.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputAttackButtonPanel : MonoBehaviour
+{
+    [SerializeField]
+    private Button attackButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+}
